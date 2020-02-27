23ef1fb7ed703d629353d46392b6508f1a3cab27
Javin Hung <kilikafinal@gmail.com>
2020-02-26 23:07:41 -0800

    quiz initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'Whats your favorite color?',
+        'answers': ['Blue', 'Red', 'Green', 'Yellow'],
+      },
+      {
+        'questionText': 'Whats your favorite number?',
+        'answers': ['One', 'Two', 'Three', 'Four'],
+      },
+      {
+        'questionText': 'Whats your name?',
+        'answers': ['Ken', 'Jimothy', 'Alexander', 'Scooter'],
+      },
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
+      ),
+    );
+  }
+}

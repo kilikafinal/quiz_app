ee94c850ac40ccb3d0623aba01d3101115bf2d06
Javin Hung <kilikafinal@gmail.com>
2020-03-01 15:55:28 -0800

    Added quiz reset functionality

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,13 @@ class _MyAppState extends State<MyApp> {
 
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+    _questionIndex = 0;
+    _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
     setState(() {
@@ -71,7 +78,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questions: _questions,
                 questionIndex: _questionIndex)
-            : Result(),
+            : Result("hello",_resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final String _resultScore;
+  final Function resetQuiz;
+
+  Result(this._resultScore, this.resetQuiz);
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("woo!!"),
+      child: Column(
+        children: <Widget>[
+          Text(_resultScore),
+          FlatButton(
+            child: Text("reset quiz"),
+            onPressed: resetQuiz,
+          ),
+        ],
+      ),
     );
   }
 }
